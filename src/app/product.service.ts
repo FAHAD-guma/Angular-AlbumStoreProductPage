a9d1ddf37511e2f4822945d4e11c5ac184d3d8d8
Software Engineer, FAHAD Guma <56593325+FAHAD-guma@users.noreply.github.com>
2020-10-05 21:41:05 +0300

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,12 +7,12 @@ import  'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private _albumURL = '../assetsalbum.json';
+  private _albumUrl = '../assetsalbum.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumURL).map(response => response.json())
+    return this._http.get(this._albumUrl).map(response => response.json())
 
   }
 
