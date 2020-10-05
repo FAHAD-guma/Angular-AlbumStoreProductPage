72584157c2b2ab75411d52a6eac2a3bd9d9ef3f2
Software Engineer, FAHAD Guma <56593325+FAHAD-guma@users.noreply.github.com>
2020-10-05 23:55:47 +0300

    Completed

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Album } from './album'
 
 import  'rxjs/add/operator/map';
 
